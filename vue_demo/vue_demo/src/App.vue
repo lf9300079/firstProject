3c4b4e896e74f10be2897ca3010502c860b64c43
louis <1074467699@qq.com>
2019-08-13 11:48:57 +0800

    提交vue工程到本地仓库

---
+++ vue_demo/vue_demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--3.使用该组件标签-->
+    <HelloWorld1/>
+   <!-- <router-view/>-->
+  </div>
+</template>
+
+<script>
+  // 1.引入组件
+  import HelloWorld1 from './components/HelloWorld1'
+
+export default {
+  name: 'App',
+  components: {
+    //2.映射组件标签
+    HelloWorld1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_demo/vue_demo/src/components/HelloWorld1.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+<script>
+  // import __ from '__' // __是需要手动引入的文件
+  export default {
+    name: 'HelloWorld',
+    components: {},
+    //必须写入一个函数
+      data () {
+          return {msg: '这个是Home模板页'}
+      },
+      methods: {}
+  }
+</script>
+<style lang="postcss" scoped>
+  body {
+      background-color: #ff0000;
+  }
+</style>
